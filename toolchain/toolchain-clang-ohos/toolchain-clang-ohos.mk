c087a265d42cca2421e0c0216a3ffca104b9efa0
Francesco Pham <francesco.pham@huawei.com>
2024-12-16 16:00:14 +0100

    install external musl sysroot
    Signed-off-by: Francesco Pham <francesco.pham@huawei.com>

--- toolchain/toolchain-clang-ohos/toolchain-clang-ohos.mk
+++ toolchain/toolchain-clang-ohos/toolchain-clang-ohos.mk
@@ -48,11 +48,38 @@ define TOOLCHAIN_CLANG_EXTERNAL_INSTALL_SYSROOT
 	$(Q)cp -a $(TOOLCHAIN_CLANG_EXTERNAL_SYSROOT)/* $(STAGING_DIR)
 endef
 
+define TOOLCHAIN_CLANG_EXTERNAL_INSTALL_MUSL_SYSROOT
+	$(Q)$(call MESSAGE,"Copying MUSL sysroot to staging...")
+	@if [ -z "$(BR2_TOOLCHAIN_EXTERNAL_MUSL_SYSROOT_DIR)" ]; then \
+		echo "Error: BR2_TOOLCHAIN_EXTERNAL_MUSL_SYSROOT_DIR is not set"; \
+		exit 1; \
+	fi
+	$(Q)cp -a $(BR2_TOOLCHAIN_EXTERNAL_MUSL_SYSROOT_DIR)/usr/* $(STAGING_DIR)/usr/
+endef
+
+TOOLCHAIN_CLANG_OHOS_TOOLCHAIN_WRAPPER_ARGS += \
+	-DBR_CROSS_PATH_SUFFIX='' \
+	-DBR_CROSS_PATH_ABS='"$(TOOLCHAIN_CLANG_EXTERNAL_BIN)"'
+
 # Install toolchain wrappers and symlinks
 
 define TOOLCHAIN_CLANG_EXTERNAL_INSTALL_WRAPPER
-	$(Q)$(call MESSAGE,"Copying Clang toolchain binaries to host bin directory...")
-	$(Q)cp -a $(TOOLCHAIN_CLANG_EXTERNAL_BIN)/* $(HOST_DIR)/bin/
+	$(Q)$(call MESSAGE,"Creating symbolic links for Clang toolchain in host bin directory...")
+	$(Q)cd $(HOST_DIR)/bin; \
+	for i in $(TOOLCHAIN_CLANG_EXTERNAL_BIN)/*; do \
+		base=$${i##*/}; \
+		case "$$base" in \
+		*-ar|*-ranlib|*-nm) \
+			ln -sf $$(echo $$i | sed 's%^$(HOST_DIR)%..%') .; \
+			;; \
+		*clang|*clang-*|*++|*++-*|*cpp) \
+			ln -sf toolchain-wrapper $$base; \
+			;; \
+		*) \
+			ln -sf $$(echo $$i | sed 's%^$(HOST_DIR)%..%') .; \
+			;; \
+		esac; \
+	done
 endef
 
 # Generic package for Clang external toolchain
@@ -60,8 +87,14 @@ endef
 TOOLCHAIN_CLANG_OHOS_INSTALL_STAGING = YES
 TOOLCHAIN_CLANG_OHOS_ADD_TOOLCHAIN_DEPENDENCY = NO
 
+define TOOLCHAIN_CLANG_OHOS_BUILD_CMDS
+	$(TOOLCHAIN_WRAPPER_BUILD)
+endef
+
 define TOOLCHAIN_CLANG_OHOS_INSTALL_STAGING_CMDS
+	$(TOOLCHAIN_WRAPPER_INSTALL)
 	$(TOOLCHAIN_CLANG_EXTERNAL_INSTALL_SYSROOT)
+	$(TOOLCHAIN_CLANG_EXTERNAL_INSTALL_MUSL_SYSROOT)
 	$(TOOLCHAIN_CLANG_EXTERNAL_INSTALL_WRAPPER)
 endef
 
